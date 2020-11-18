d3ab6a05534be2a822eb6ca674533ca0dd1e02de
hannahjorich <hannah.jorich@gmail.com>
2020-11-17 18:27:24 -0700

    rendering info

--- client/src/pages/Books.js
+++ client/src/pages/Books.js
@@ -7,14 +7,16 @@ class Books extends Component {
   state = {
     books: [],
     term: "",
+    Message: "Search for a book to begin",
   };
   // Loads all books and sets them to books
   loadBooks = () => {
     API.getBooks(this.state.term)
       .then((res) =>
-        this.setState({
+        this.setState(prevstate => ({
+          ...prevstate,
           books: res.data,
-        })
+        }))
       )
       .catch((err) => console.log(err));
   };
@@ -25,18 +27,35 @@ class Books extends Component {
   };
 
   handleFormSubmit = (event) => {
-      event.preventDefault();
-      this.loadBooks(this.state.term)
-  }
+    event.preventDefault();
+    this.loadBooks(this.state.term);
+  };
+
+  handleBookSave = (id) => {
+    const book = this.state.books.find((book) => book.id === id);
+  };
 
   render() {
-      return (
-      <Card><Form
-        handleInputChange={this.handleInputChange}
-        handleFormSubmit={this.handleFormSubmit}
-        term={this.state.term}
-      ></Form></Card>
-      )
+    return (
+      <div>
+        <Card>
+          <Form
+            handleInputChange={this.handleInputChange}
+            handleFormSubmit={this.handleFormSubmit}
+            term={this.state.term}
+          ></Form>
+          {this.state.books.items && console.log(this.state.books.items[0].volumeInfo)}
+        </Card>
+        {this.state.books.items && this.state.books.items.map((book) => (
+          <Card>
+            {book.volumeInfo.imageLinks.smallThumbnail}
+            <h2>{book.volumeInfo.title}</h2>
+            <p>{book.volumeInfo.authors}</p>
+            <p>{book.volumeInfo.description}</p>
+          </Card>
+        ))}
+      </div>
+    );
   }
 }
 
